0132ea68f519aad1c65890ad04a78bf7bcd64401
rizqekaputra <rizqekaputra353@gmail.com>
2020-09-30 08:33:39 +0700

    update domevent

---
+++ main.js
@@ -0,0 +1,35 @@
+
+//kita membuat sebuah fungsi yang akan
+//memunculkan MODAL
+
+const showModal = ()=>{
+
+        //kita ambil element yang akan di SHOW
+        let modal = document.querySelector('.modalMenu')
+
+        //kita atur style dari element tersebut
+        //jadikan dia terlihat
+        modal.style.display = 'flex'
+}
+
+//kita buat fungsi untuk hide element
+const hideModal = ()=>{
+
+    //kita ambil elment yang akan di SHOW
+    let modal = document.
+    querySelector('.modalMenu')
+
+    //kita atur STYLE dari element tersebut
+    //jadikan dia tersembunyi
+    modal.style.display = 'none'
+}
+
+//kita buat fungsi hide alert
+const hideaboutAlert = ()=>{
+    //kita ambil elment yang akan di SHOW
+    let aboutAlert = document.querySelector('.aboutAlert')
+
+    //kita atur style dari element tersebut
+        //jadikan dia terlihat
+        aboutAlert.style.display ='none'
+}
